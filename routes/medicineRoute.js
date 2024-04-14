f5d9d49b71e89cd9b6abc35e6d64bd6a0e576a5e
lamnguyen <ln14072004@gmail.com>
2024-04-14 11:50:41 +0700

    update

--- routes/medicineRoute.js
+++ routes/medicineRoute.js
@@ -4,10 +4,11 @@ const authenticate = require("../lib/auth");
 const router = express.Router();
 
 router.get("/getData", medicineController.findMedicines);
-router.get("/getExp",medicineController.findMedicinesExpire);
-router.get("/getDetail/:id",medicineController.findMedicineDetail);
-router.post("/create",medicineController.createMedicine);
-router.delete("/delete/:id",medicineController.removeMedicine);
+router.get("/getExp", medicineController.findMedicinesExpire);
+router.get("/getDetail/:id", medicineController.findMedicineDetail);
+router.post("/create", medicineController.createMedicine);
+router.delete("/delete/:id", medicineController.removeMedicine);
+
 
 
 module.exports = router;
